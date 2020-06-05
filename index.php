e5f4bfc9dea07c20ba3694d10888515b1ad73fc7
ThomasMurat <thomasmurat1992@gmail.com>
2020-06-05 11:23:27 +0200

    up exo2

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Php-Partie1-exercice1</title>
+    </head>
+    <body>
+        <?php 
+            $firstname = 'Thomas';
+            $lastname = 'Murat';
+            $age = 20;
+            echo "$firstname, $lastname, $age";
+        ?>
+    </body>
+</html>
